0520a176b4675066df54f759b3ed4e28776296fe
MohamedAboElAla <mohamed20300005@gmail.com>
2025-05-12 19:54:02 +0300

    Add project files.

---
+++ C#AdvDay1/Complex.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_AdvDay1
+{
+    internal class Complex
+    {
+        int real;
+        int img;
+        public int Real
+        {
+            get => real; 
+            set => real = value;
+        }
+        public int Img
+        {
+            get => img;
+            set => img = value;
+        }
+
+        public Complex()
+        {
+            
+        }
+        public Complex(int _real, int _img)
+        {
+            real = _real;
+            img = _img;
+        }
+
+        // Overloading the + operator
+        public static Complex operator +(Complex c1,Complex c2)
+        =>new Complex(c1.real + c2.real, c1.img + c2.img);
+
+        // Overloading the - operator
+        public static Complex operator -(Complex c1, Complex c2)
+            => new Complex(c1.real - c2.real, c1.img - c2.img);
+
+        // Overloading the * operator
+        public static Complex operator * (Complex c1, Complex c2)
+        => new Complex(c1.real * c2.real - c1.img * c2.img, c1.real * c2.img + c1.img * c2.real);
+
+        // Overloading the / operator
+        public static Complex operator /(Complex c1, Complex c2)
+        {
+            int denominator = c2.real * c2.real + c2.img * c2.img;
+            if (denominator == 0)
+            {
+                Console.WriteLine("Cannot divide by zero");
+                return new Complex(0,0);
+            }
+            return new Complex((c1.real * c2.real + c1.img * c2.img) / denominator, (c1.img * c2.real - c1.real * c2.img) / denominator);
+        }
+        //  Overloading the % operator
+        public static Complex operator %(Complex c1, Complex c2)
+        {
+            int denominator = c2.real * c2.real + c2.img * c2.img;
+            if (denominator == 0)
+            {
+                Console.WriteLine("Cannot divide by zero");
+                return new Complex(0, 0);
+            }
+            return new Complex((c1.real * c2.real + c1.img * c2.img) % denominator, (c1.img * c2.real - c1.real * c2.img) % denominator);
+        }
+        // Overloading the == operator
+        public static bool operator ==(Complex c1, Complex c2)
+         => c1.real == c2.real && c1.img == c2.img;
+
+        // Overloading the != operator
+        public static bool operator !=(Complex c1, Complex c2)
+         => !(c1 == c2);
+
+        // Overloading the < operator
+        public static bool operator <(Complex c1, Complex c2)
+         => (c1.real * c1.real + c1.img * c1.img) < (c2.real * c2.real + c2.img * c2.img);
+        // Overloading the > operator
+        public static bool operator >(Complex c1, Complex c2)
+         => (c1.real * c1.real + c1.img * c1.img) > (c2.real * c2.real + c2.img * c2.img);
+
+        // Overloading the <= operator
+        public static bool operator <=(Complex c1, Complex c2)
+         => (c1.real * c1.real + c1.img * c1.img) <= (c2.real * c2.real + c2.img * c2.img);
+
+        // Overloading the >= operator
+        public static bool operator >=(Complex c1, Complex c2)
+         => (c1.real * c1.real + c1.img * c1.img) >= (c2.real * c2.real + c2.img * c2.img);
+
+        // Overloading the ++ operator
+        public static Complex operator ++(Complex c)
+        {
+            c.real++;
+            return c;
+        }
+        // Overloading the -- operator
+        public static Complex operator --(Complex c)
+        {
+            c.real--;
+            return c;
+        }
+
+       public static Complex operator +(Complex c , int right)
+       => new Complex(c.real + right, c.img);
+
+        public static Complex operator +(int left, Complex c)
+       => new Complex(left + c.real,c.img); 
+
+
+
+
+
+      public void Print()
+        {
+
+            if(img<0)
+            Console.WriteLine($"{real} {img}i");
+            else if(img>0)
+                Console.WriteLine($"{real} + {img}i");
+            else
+                Console.WriteLine($"{real}");
+        }
+
+
+    }
+
+    internal class abc : Complex
+    {
+
+        public abc(int _real, int _img) : base(_real, _img)
+        {
+        }
+        public void Print()
+        {
+            Console.WriteLine("abc");
+        }
+    }
+}
---
+++ C#AdvDay1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace C_AdvDay1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Complex c1= new Complex(2, 3);
+            //Complex c2 = new Complex(4, 5);
+            //Complex c3 = c1 - c2;
+            //c3.Print();
+            Complex c1 = new Complex(2, 3);
+            ++c1;
+            c1.Print();
+
+
+
+            //abc a = new abc(1, 2);
+            //abc b = new abc(3, 4);
+            //Complex result = a - b;  
+            //result.Print();          
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
